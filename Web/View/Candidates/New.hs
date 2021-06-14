ba46c1684fa63fd101374d9668104311d14e6006
Marcos Tirao <marcos.tirao@icloud.com>
2021-06-14 18:01:53 -0300

    View fixes

--- Web/View/Candidates/New.hs
+++ Web/View/Candidates/New.hs
@@ -13,7 +13,6 @@ instance View NewView where
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                    <li class="breadcrumb-item active">New Candidate</li>
                     <li class="nav-item active">
                         <a class="nav-link" href="/Candidates">Candidates</a>
                     </li>
